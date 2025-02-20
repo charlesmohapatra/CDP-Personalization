d5e33693fc2c38a63ddcf902de82822f4fc452b1
Sarthak Das <53120248+charlesmohapatra@users.noreply.github.com>
2025-02-20 17:26:35 +0530

    Update HubspotForm.js

--- src/components/HubspotForm.js
+++ src/components/HubspotForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import { AnalyticsBrowser } from "@segment/analytics-next";
 const writeKey = process.env.write_key;
 const analytics = AnalyticsBrowser.load({ writeKey: "trt2mhv6rjiqM8rpsRExWM1pBiguWqUm" });
